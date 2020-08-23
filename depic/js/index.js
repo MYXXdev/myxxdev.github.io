6c512da3c59a2a0f5a091d8496724f8b964c2643
MYXXdev <myxxdev@myxxfm.com>
2020-08-23 03:25:50 -0500

    MYXXdev

---
+++ depic/js/index.js
@@ -0,0 +1,1 @@
+(function(a){"use strict";navigator.userAgent.indexOf("Cydia")!=-1?(a.title=a.title.split(" \u00b7 ")[0],a.documentElement.classList.add("cydia")):a.documentElement.classList.remove("cydia","depiction")})(document)
