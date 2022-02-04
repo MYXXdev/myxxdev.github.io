59a242c46e397d05985489df220e6009db6a205a
MYXXdev <myxxdev@myxxfm.com>
2022-02-04 15:50:07 -0700

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-dpkg-scanpackages ./debs > Packages
-bzip2 -fks Packages
+dpkg-scanpackages -m debs/ /dev/null > Packages && bzip2 -c9 Packages > Packages.bz2 && gzip -c9 Packages > Packages.gz
