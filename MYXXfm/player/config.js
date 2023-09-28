f61016cb73ff3fe00a42a1fab29de875f6fb972f
MYXXdev <myxxdev@myxxfm.com>
2023-09-27 22:06:28 -0600

    Update config.js

--- MYXXfm/player/config.js
+++ MYXXfm/player/config.js
@@ -9,7 +9,7 @@
 */
 
 var settings = {
-    'radio_name': 'MYXX Hits 1',
+    'radio_name': 'MYXX Hits 2',
 
     // URL of streaming without / on the final, eg: http://streaming.com:8080
     'url_streaming': 'https://rbx2.hnux.com/http://e15446.cloudrad.io:10934/live',
