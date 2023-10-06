9bf08390f51d0b09d1f0a27c90e844a2bec60dcc
MYXXdev <myxxdev@myxxfm.com>
2023-10-05 18:35:46 -0600

    MYXXfm

---
+++ MYXXfm/player-hq/js/script.js
@@ -0,0 +1,491 @@
+const RADIO_NAME = 'MYXX FM | Spanglish Hits Live Here';
+var API_SERVICE = 'DEEZER';
+const URL_STREAMING = 'https://forwardmystream.com/myxxfmhq';
+const API_URL = 'https://api.streamafrica.net/metadata/index.php?z='+URL_STREAMING
+const API_KEY = "18fe07917957c289983464588aabddfb";
+
+window.onload = function () {
+    var page = new Page;
+    page.changeTitlePage();
+    page.setVolume();
+
+    var player = new Player();
+    player.play();
+
+    getStreamingData();
+    setInterval(function () {
+        getStreamingData();
+    }, 10000);
+
+    var coverArt = document.getElementsByClassName('cover-album')[0];
+
+    coverArt.style.height = coverArt.offsetWidth + 'px';
+}
+
+function Page() {
+    this.changeTitlePage = function (title = RADIO_NAME) {
+        document.title = title;
+    };
+
+    this.refreshCurrentSong = function (song, artist) {
+        var currentSong = document.getElementById('currentSong');
+        var currentArtist = document.getElementById('currentArtist');
+
+        if (song !== currentSong.innerHTML) {
+            currentSong.className = 'animated flipInY text-uppercase';
+            currentSong.innerHTML = song;
+
+            currentArtist.className = 'animated flipInY text-capitalize';
+            currentArtist.innerHTML = artist;
+
+            document.getElementById('lyricsSong').innerHTML = song + ' - ' + artist;
+
+            setTimeout(function () {
+                currentSong.className = 'text-uppercase';
+                currentArtist.className = 'text-capitalize';
+            }, 2000);
+        }
+    }
+
+    this.refreshHistoric = function (info, n) {
+        var $historicDiv = document.querySelectorAll('#historicSong article');
+        var $songName = document.querySelectorAll('#historicSong article .music-info .song');
+        var $artistName = document.querySelectorAll('#historicSong article .music-info .artist');
+
+        var urlCoverArt = 'img/MYXXfm.png';
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                var data = JSON.parse(this.responseText);
+                var artworking = data.results;
+                var gotit = artworking.artwork;
+
+                document.querySelectorAll('#historicSong article .cover-historic')[n].style.backgroundImage = 'url(' + gotit + ')';
+            }
+
+            var music = info.song.replace(/&apos;/g, '\'');
+            var songHist = music.replace(/&amp;/g, '&');
+
+            var artist = info.artist.replace(/&apos;/g, '\'');
+            var artistHist = artist.replace(/&amp;/g, '&');
+
+            $songName[n].innerHTML = songHist;
+            $artistName[n].innerHTML = artistHist;
+
+            $historicDiv[n].classList.add('animated');
+            $historicDiv[n].classList.add('slideInRight');
+        }
+        xhttp.open('GET', 'https://api.streamafrica.net/new.search.php?query=' + info.artist + ' ' + info.song + '&service=' + API_SERVICE.toLowerCase());
+        xhttp.send();
+
+        setTimeout(function () {
+            for (var j = 0; j < 2; j++) {
+                $historicDiv[j].classList.remove('animated');
+                $historicDiv[j].classList.remove('slideInRight');
+            }
+        }, 2000);
+    }
+
+    this.refreshCover = function (song = '', artist) {
+
+        var urlCoverArt = 'img/MYXXfm.png';
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            var coverArt = document.getElementById('currentCoverArt');
+            var coverBackground = document.getElementById('bgCover');
+
+            if (this.readyState === 4 && this.status === 200) {
+                var data = JSON.parse(this.responseText);
+                var artworkUrl100 = data.results;
+                var urlCoverArt = artworkUrl100.artwork;
+
+                coverArt.style.backgroundImage = 'url(' + urlCoverArt + ')';
+                coverArt.className = 'animated bounceInLeft';
+
+                coverBackground.style.backgroundImage = 'url(' + urlCoverArt + ')';
+
+                setTimeout(function () {
+                    coverArt.className = '';
+                }, 2000);
+
+                if ('mediaSession' in navigator) {
+                    navigator.mediaSession.metadata = new MediaMetadata({
+                        title: song,
+                        artist: artist,
+                        artwork: [{
+                                src: urlCoverArt,
+                                sizes: '96x96',
+                                type: 'image/png'
+                            },
+                            {
+                                src: urlCoverArt,
+                                sizes: '128x128',
+                                type: 'image/png'
+                            },
+                            {
+                                src: urlCoverArt,
+                                sizes: '192x192',
+                                type: 'image/png'
+                            },
+                            {
+                                src: urlCoverArt,
+                                sizes: '256x256',
+                                type: 'image/png'
+                            },
+                            {
+                                src: urlCoverArt,
+                                sizes: '384x384',
+                                type: 'image/png'
+                            },
+                            {
+                                src: urlCoverArt,
+                                sizes: '512x512',
+                                type: 'image/png'
+                            }
+                        ]
+                    });
+                }
+            }
+        }
+        xhttp.open('GET', 'https://api.streamafrica.net/new.search.php?query=' + artist + ' ' + song + '&service=' + API_SERVICE.toLowerCase());
+        xhttp.send();
+    }
+
+    this.changeVolumeIndicator = function (volume) {
+        document.getElementById('volIndicator').innerHTML = volume;
+
+        if (typeof (Storage) !== 'undefined') {
+            localStorage.setItem('volume', volume);
+        }
+    }
+
+    this.setVolume = function () {
+        if (typeof (Storage) !== 'undefined') {
+            var volumeLocalStorage = (!localStorage.getItem('volume')) ? 80 : localStorage.getItem('volume');
+            document.getElementById('volume').value = volumeLocalStorage;
+            document.getElementById('volIndicator').innerHTML = volumeLocalStorage;
+        }
+    }
+
+    this.refreshLyric = function (currentSong, currentArtist) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                var data = JSON.parse(this.responseText);
+
+                var openLyric = document.getElementsByClassName('lyrics')[0];
+
+                if (data.type === 'exact' || data.type === 'aprox') {
+                    var lyric = data.mus[0].text;
+
+                    document.getElementById('lyric').innerHTML = lyric.replace(/\n/g, '<br />');
+                    openLyric.style.opacity = "1";
+                    openLyric.setAttribute('data-toggle', 'modal');
+                } else {
+                    openLyric.style.opacity = "0.3";
+                    openLyric.removeAttribute('data-toggle');
+
+                    var modalLyric = document.getElementById('modalLyrics');
+                    modalLyric.style.display = "none";
+                    modalLyric.setAttribute('aria-hidden', 'true');
+                    (document.getElementsByClassName('modal-backdrop')[0]) ? document.getElementsByClassName('modal-backdrop')[0].remove(): '';
+                }
+            } else {
+                document.getElementsByClassName('lyrics')[0].style.opacity = "0.3";
+                document.getElementsByClassName('lyrics')[0].removeAttribute('data-toggle');
+            }
+        }
+        xhttp.open('GET', 'https://api.vagalume.com.br/search.php?apikey=' + API_KEY + '&art=' + currentArtist + '&mus=' + currentSong.toLowerCase(), true);
+        xhttp.send()
+    }
+}
+
+var audio = new Audio(URL_STREAMING);
+
+function Player() {
+    this.play = function () {
+        audio.play();
+
+        var defaultVolume = document.getElementById('volume').value;
+
+        if (typeof (Storage) !== 'undefined') {
+            if (localStorage.getItem('volume') !== null) {
+                audio.volume = intToDecimal(localStorage.getItem('volume'));
+            } else {
+                audio.volume = intToDecimal(defaultVolume);
+            }
+        } else {
+            audio.volume = intToDecimal(defaultVolume);
+        }
+        document.getElementById('volIndicator').innerHTML = defaultVolume;
+    };
+
+    this.pause = function () {
+        audio.pause();
+    };
+}
+
+audio.onplay = function () {
+    var botao = document.getElementById('playerButton');
+    var bplay = document.getElementById('buttonPlay');
+    if (botao.className === 'fa fa-play') {
+        botao.className = 'fa fa-pause';
+        bplay.firstChild.data = 'PAUSE';
+    }
+}
+
+audio.onpause = function () {
+    var botao = document.getElementById('playerButton');
+    var bplay = document.getElementById('buttonPlay');
+    if (botao.className === 'fa fa-pause') {
+        botao.className = 'fa fa-play';
+        bplay.firstChild.data = 'PLAY';
+    }
+}
+
+audio.onvolumechange = function () {
+    if (audio.volume > 0) {
+        audio.muted = false;
+    }
+}
+
+audio.onerror = function () {
+    var confirmacao = confirm('Stream Down / Network Error. \nClick OK to try again.');
+
+    if (confirmacao) {
+        window.location.reload();
+    }
+}
+
+document.getElementById('volume').oninput = function () {
+    audio.volume = intToDecimal(this.value);
+
+    var page = new Page();
+    page.changeVolumeIndicator(this.value);
+}
+
+function togglePlay() {
+    if (!audio.paused) {
+        audio.pause();
+    } else {
+        audio.load();
+        audio.play();
+    }
+}
+
+function volumeUp() {
+    var vol = audio.volume;
+    if(audio) {
+        if(audio.volume >= 0 && audio.volume < 1) {
+            audio.volume = (vol + .01).toFixed(2);
+        }
+    }
+}
+
+function volumeDown() {
+    var vol = audio.volume;
+    if(audio) {
+        if(audio.volume >= 0.01 && audio.volume <= 1) {
+            audio.volume = (vol - .01).toFixed(2);
+        }
+    }
+}
+
+function mute() {
+    if (!audio.muted) {
+        document.getElementById('volIndicator').innerHTML = 0;
+        document.getElementById('volume').value = 0;
+        audio.volume = 0;
+        audio.muted = true;
+    } else {
+        var localVolume = localStorage.getItem('volume');
+        document.getElementById('volIndicator').innerHTML = localVolume;
+        document.getElementById('volume').value = localVolume;
+        audio.volume = intToDecimal(localVolume);
+        audio.muted = false;
+    }
+}
+
+function getStreamingData() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+
+        if (this.readyState === 4 && this.status === 200) {
+
+            if(this.response.length === 0) {
+                console.log('%cdebug', 'font-size: 22px')
+            }
+
+            var data = JSON.parse(this.responseText);
+
+            var page = new Page();
+
+            // Formating characters to UTF-8
+            let song = data.currentSong.replace(/&apos;/g, '\'');
+            currentSong = song.replace(/&amp;/g, '&');
+
+            let artist = data.currentArtist.replace(/&apos;/g, '\'');
+            currentArtist = artist.replace(/&amp;/g, '&');
+
+            // Change the title
+            document.title = currentSong + ' - ' + currentArtist + ' | ' + RADIO_NAME;
+
+            if (document.getElementById('currentSong').innerHTML !== song) {
+                page.refreshCover(currentSong, currentArtist);
+                page.refreshCurrentSong(currentSong, currentArtist);
+                page.refreshLyric(currentSong, currentArtist);
+
+                for (var i = 0; i < 2; i++) {
+                    page.refreshHistoric(data.songHistory[i], i);
+                }
+            }
+        } 
+    };
+
+    var d = new Date();
+
+    xhttp.open('GET', API_URL);
+    xhttp.send();
+}
+
+document.addEventListener('keydown', function (k) {
+    var k = k || window.event;
+    var key = k.keyCode || k.which;
+    
+    var slideVolume = document.getElementById('volume');
+
+    var page = new Page();
+
+    switch (key) {
+
+        case 38:
+            volumeUp();
+            slideVolume.value = decimalToInt(audio.volume);
+            page.changeVolumeIndicator(decimalToInt(audio.volume));
+            break;
+        case 40:
+            volumeDown();
+            slideVolume.value = decimalToInt(audio.volume);
+            page.changeVolumeIndicator(decimalToInt(audio.volume));
+            break;
+        case 32:
+            togglePlay();
+            break;
+        case 80:
+            togglePlay();
+            break;
+        case 77:
+            mute();
+            break;
+        case 48:
+            audio.volume = 0;
+            slideVolume.value = 0;
+            page.changeVolumeIndicator(0);
+            break;
+        case 96:
+            audio.volume = 0;
+            slideVolume.value = 0;
+            page.changeVolumeIndicator(0);
+            break;
+        case 49:
+            audio.volume = .1;
+            slideVolume.value = 10;
+            page.changeVolumeIndicator(10);
+            break;
+        case 97:
+            audio.volume = .1;
+            slideVolume.value = 10;
+            page.changeVolumeIndicator(10);
+            break;
+        case 50:
+            audio.volume = .2;
+            slideVolume.value = 20;
+            page.changeVolumeIndicator(20);
+            break;
+        case 98:
+            audio.volume = .2;
+            slideVolume.value = 20;
+            page.changeVolumeIndicator(20);
+            break;
+        case 51:
+            audio.volume = .3;
+            slideVolume.value = 30;
+            page.changeVolumeIndicator(30);
+            break;
+        case 99:
+            audio.volume = .3;
+            slideVolume.value = 30;
+            page.changeVolumeIndicator(30);
+            break;
+        case 52:
+            audio.volume = .4;
+            slideVolume.value = 40;
+            page.changeVolumeIndicator(40);
+            break;
+        case 100:
+            audio.volume = .4;
+            slideVolume.value = 40;
+            page.changeVolumeIndicator(40);
+            break;
+        case 53:
+            audio.volume = .5;
+            slideVolume.value = 50;
+            page.changeVolumeIndicator(50);
+            break;
+        case 101:
+            audio.volume = .5;
+            slideVolume.value = 50;
+            page.changeVolumeIndicator(50);
+            break;
+        case 54:
+            audio.volume = .6;
+            slideVolume.value = 60;
+            page.changeVolumeIndicator(60);
+            break;
+        case 102:
+            audio.volume = .6;
+            slideVolume.value = 60;
+            page.changeVolumeIndicator(60);
+            break;
+        case 55:
+            audio.volume = .7;
+            slideVolume.value = 70;
+            page.changeVolumeIndicator(70);
+            break;
+        case 103:
+            audio.volume = .7;
+            slideVolume.value = 70;
+            page.changeVolumeIndicator(70);
+            break;
+        case 56:
+            audio.volume = .8;
+            slideVolume.value = 80;
+            page.changeVolumeIndicator(80);
+            break;
+        case 104:
+            audio.volume = .8;
+            slideVolume.value = 80;
+            page.changeVolumeIndicator(80);
+            break;
+        case 57:
+            audio.volume = .9;
+            slideVolume.value = 90;
+            page.changeVolumeIndicator(90);
+            break;
+        case 105:
+            audio.volume = .9;
+            slideVolume.value = 90;
+            page.changeVolumeIndicator(90);
+            break;
+    }
+});
+
+function intToDecimal(vol) {
+    return vol / 100;
+}
+
+function decimalToInt(vol) {
+    return vol * 100;
+}
